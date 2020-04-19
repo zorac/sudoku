d3fdac42103cf496ce7eaca69ece875fb69580e9
Mark Rigby-Jones <mark@rigby-jones.net>
2020-04-19 13:39:36 +0100

    Build for GitHub Pages

---
+++ precache-manifest.307e0b788beb80364e0335e5d1c4bc7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f6a1a5bfc435ddfbd4816fc611de2a",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "0b492ba36aab496f9a44",
+    "url": "/sudoku/static/css/main.188b766a.chunk.css"
+  },
+  {
+    "revision": "7391d3933d34e726039a",
+    "url": "/sudoku/static/js/2.3e2a8725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.3e2a8725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b492ba36aab496f9a44",
+    "url": "/sudoku/static/js/main.49ec4eda.chunk.js"
+  },
+  {
+    "revision": "7f2f73783f1baa48b949",
+    "url": "/sudoku/static/js/runtime-main.e0cfc196.js"
+  }
+]);
